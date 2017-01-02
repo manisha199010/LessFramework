9d9fc494065a44c7302ed12d54e88bad1861c83b
Manisha Bano <manisha.bano@intelligrape.com>
2017-01-02 15:15:06 +0530

    first commit

---
+++ bundle/src/test/java/com/framework/less/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.framework.less;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
